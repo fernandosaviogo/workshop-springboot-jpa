9354df7206fdbac0049b8405fc669e5835b769b0
Fernando Sávio Gomes <133050259+fernandosaviogo@users.noreply.github.com>
2025-04-29 15:37:38 -0300

    Payment, one-to-one association

--- src/main/java/com/fernandoweb/course/entities/Order.java
+++ src/main/java/com/fernandoweb/course/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fernandoweb.course.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -39,6 +41,9 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")   // O id do OrderItem que tem o pedido
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)  // No caso do 1 para 1 com o mesmo idm você coloca o cascade = CascadeType.ALL
+	private Payment payment;
 	
 	// Metodo construtor vazio
 	public Order() {
@@ -92,7 +97,15 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
-	
+		
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	// Metodo hasCode
 	@Override
 	public int hashCode() {
---
+++ src/main/java/com/fernandoweb/course/entities/Payment.java
@@ -0,0 +1,87 @@
+package com.fernandoweb.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	// Variaveis
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant moment;
+	
+	@OneToOne
+	@MapsId   //usada para especificar que o identificador de entidade é mapeado pelo @OneToOne associado.
+	private Order order;
+	
+	// Metodo construtor vazio
+	public Payment() {
+		
+	}
+	
+	// Metodo construtor
+	public Payment(Long id, Instant moment, Order order) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.order = order;
+	}
+	
+	// Metodos Gets e Sets
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+	// Metodo hashCode
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	// metodo equals
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
